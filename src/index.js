100d443e41b25cbf516bca8e396d42845f3a1190
Abasiomofon Udoh <omofon.u@gmail.com>
2025-12-12 07:47:56 +0100

    feat(html): Complete syntax for form

---
+++ src/index.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const errorDict = {
+    firstName: {
+      empty: "This field is required",
+    },
+    lastName: {
+      empty: "This field is required",
+    },
+    email: {
+      empty: "This field is required",
+      invalid: "Please enter a valid email address",
+    },
+    queryType: {
+      empty: "Please select a query type",
+    },
+    message: {
+      empty: "This field is required",
+    },
+    consent: {
+      empty: "To submit this form, please consent to being contacted",
+    },
+  };
+});
